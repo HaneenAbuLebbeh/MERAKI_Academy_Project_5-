1fd7814c2622a2683dbf36b0910d95dd289c8847
C12-SmadiAhmed <ahmed.f.smadi@gmail.com>
2024-11-09 16:08:23 +0300

    register&login

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,13 +2,25 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 require("./models/db");
-
-
 const app = express();
+
+
+//routers 
+const userRouter=require("./routes/users")
+
 
 //built-in middleware
 app.use(express.json());
 app.use(cors());
+
+// router middleware 
+app.use("/users",userRouter)
+
+
+ 
+
+
+
 
 
 const PORT = process.env.PORT 
