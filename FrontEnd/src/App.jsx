1a7a99429802b102282918567a25f92ace067f03
Haneen abulebbeh <haneenabulebbeh@gmail.com>
2024-11-12 22:54:21 +0300

    editign name of the website

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
     <Route path="/" element={<HomePage/>}/>
     <Route path="/Register" element={<Register/>}/>
     <Route path="/Login" element={<Login/>}/>
+    
     <Route path="/adminPanel" element={<AdminPanel/>} />
 
     </Routes>
