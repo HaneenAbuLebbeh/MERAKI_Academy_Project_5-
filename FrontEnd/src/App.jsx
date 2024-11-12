84714b12de47ee298bec485ccbe683d4a9b61e4f
C12-SmadiAhmed <ahmed.f.smadi@gmail.com>
2024-11-12 22:43:16 +0300

    stage2

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -5,7 +5,14 @@ import Login from './components/Login/Login'
 import Register from "./components/Register/Register"
 import HomePage from './components/HomrPage.jsx/HomePage'
 import AdminPanel from "./components/AdminPanel/AdminPanel"
-
+import TouristSpots from './components/TouristSpots/TouristSpots'
+import TouristSpotsDeatils from './components/TouristSpots/TouristSpotsDeatils'
+import Cart from './components/Cart/cart'
+import Products from './components/Products/Products'
+import ProductDetailes from './components/ProductsDetailes/ProductDetailes'
+import Orders from './components/Orders/Orders'
+import OrderConfirmtion from './components/Orders/OrderConfirmtion'
+import CheckOutpage from './components/CheckoutPage/CheckOutpage'
 function App() {
 
 
@@ -18,6 +25,14 @@ function App() {
     <Route path="/Register" element={<Register/>}/>
     <Route path="/Login" element={<Login/>}/>
     <Route path="/adminPanel" element={<AdminPanel/>} />
+    <Route path="/TouristSpots" element={<TouristSpots/>} />
+    <Route path="/TouristSpots-Detailes" element={<TouristSpotsDeatils/>} />
+    <Route path="/Cart" element={<Cart/>} />
+    <Route path="/Products/:category_id" element={<Products/>} />
+    <Route path="/Products/Detailes/:product_id" element={<ProductDetailes/>} />
+    <Route path="/my-orders" element={<Orders/>} />
+    <Route path="/Orders/:id" element={<OrderConfirmtion/>} />
+    <Route path="/checkout" element={<CheckOutpage/>} />
 
     </Routes>
     </>
---
+++ FrontEnd/src/components/TouristSpots/TouristSpotsDeatils.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TouristSpotsDeatils = () => {
+  return (
+    <div>TouristSpotsDeatils</div>
+  )
+}
+
+export default TouristSpotsDeatils 
