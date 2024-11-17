80fb8f03b56d383d3867156d080e46196cb48734
Haneen abulebbeh <haneenabulebbeh@gmail.com>
2024-11-17 22:21:37 +0300

    adding video

--- FrontEnd/src/components/HomrPage.jsx/Welcome.jsx
+++ FrontEnd/src/components/HomrPage.jsx/Welcome.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Welcome() {
   return (
-    <div>Welcome</div>
+    <>
+   <video autoPlay muted loop className="header-background" style={{ width: "60rem", height: "20rem", alignItems:"center"}}>
+          <source src="./src/assets/video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+    </>
   )
 }
 
