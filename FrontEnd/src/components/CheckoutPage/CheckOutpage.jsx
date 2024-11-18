a60c6dea176ba8981a274d2d72c398b6d31bfd5d
NooranAlOmari <nooranalomari@gmail.com>
2024-11-19 02:15:01 +0300

    Add order submission logic and form validation

--- FrontEnd/src/components/CheckoutPage/CheckOutpage.jsx
+++ FrontEnd/src/components/CheckoutPage/CheckOutpage.jsx
@@ -53,6 +53,55 @@ const CheckOutpage = () => {
   }, []);
 
 
+                                                                  gi
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      if (!Array.isArray(cart) || cart.length === 0) {
+        setError('Sorry, your cart is empty');
+        return;
+    }
+
+    if (!shippingAddress.coordinates || !shippingAddress.coordinates.latitude || !shippingAddress.coordinates.longitude) {
+        setError('Please enter address coordinates.');
+        return;
+    }
+
+    const { fullAddress, street, city, state, country } = shippingAddress;
+    if (!fullAddress || !street || !city || !state || !country) {
+        setError('Please fill in all shipping address fields.');
+        return;
+    }
+
+    const orderItems = cart.map(item => ({
+        product: item.productId,
+        quantity: item.quantity,
+        price: item.productPrice,
+    }));
+
+    try {
+        const response = await axios.post('http://localhost:5000/orders', {
+            shippingAddress,
+            paymentMethod,
+            note,
+            orderItems,
+        }, {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+        
+          if (response.data.success) {
+              dispatch(clearCart()); // Clear the cart after successful order
+              navigate(`/order/${response.data.order._id}`);
+          }
+      }
+
+      catch (err) {
+          setError('Failed to create request');
+          console.error('Error creating order:', err.response || err.message);
+      }
+  };
+
+
 
 
 return (
