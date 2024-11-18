fd085ad9423e9545b87c2214b3a15ea980d39063
NooranAlOmari <nooranalomari@gmail.com>
2024-11-19 02:08:08 +0300

    Dealing with geolocation

--- FrontEnd/src/components/CheckoutPage/CheckOutpage.jsx
+++ FrontEnd/src/components/CheckoutPage/CheckOutpage.jsx
@@ -16,9 +16,50 @@ const CheckOutpage = () => {
   const token = useSelector((state) => state.login.token);
   const cart = useSelector((state) => state.cart.items);
 
+  const [shippingAddress, setShippingAddress] = useState({
+      fullAddress: '',
+      street: '',
+      city: '',
+      state: '',
+      country: '',
+      coordinates: { latitude: 25.276987, longitude: 55.296249 }, // Default coordinates (default location)
+  });
+
+  const [paymentMethod, setPaymentMethod] = useState('');
+  const [note, setNotes] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleLocationChange = (location) => {
+      setShippingAddress((prev) => ({
+          ...prev,
+          coordinates: location,
+      }));
+  };
+
+   console.log("hi this is the cart");
+   console.log(cart);
+
+  useEffect(() => {
+      // Automatically fetch user's location on mount
+      navigator.geolocation.getCurrentPosition(
+          (position) => {
+              const { latitude, longitude } = position.coords;
+              handleLocationChange({ latitude, longitude });
+          },
+          (err) => {
+              console.error('Error fetching location:', err);
+          }
+      );
+  }, []);
+
+
+
+
 return (
-  <div></div>
- );
+  <div className="checkout-container cart-page ">
+
+  </div>
+);
 };
 
 export default CheckOutpage;
