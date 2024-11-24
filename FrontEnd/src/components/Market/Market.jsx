5c8aa78e4447c7187b2f8dd04f9abb6c9ace4fa8
NooranAlOmari <nooranalomari@gmail.com>
2024-11-24 15:14:40 +0300

    Display Market Products with Details & Implement Favorite Toggle

--- FrontEnd/src/components/Market/Market.jsx
+++ FrontEnd/src/components/Market/Market.jsx
@@ -37,12 +37,12 @@ const Market = () => {
     }, [dispatch]);
 
 
-    // Navigate to product-details when the image is clicked
+    //Navigate to product-details when the image is clicked
     const handleProductClick = (productId) => {
         navigate(`/market/details/${productId}`);
     };
 
-    // Toggle favorite 
+    //Toggle favorite 
     const toggleFavoriteHandler = (productId) => {
         dispatch(toggleFavorite(productId));
     };
@@ -50,7 +50,47 @@ const Market = () => {
 
     return (
         <section className="alll-cards">
-        
+            {isLoading ? (
+                <div className="loading-indicator">
+                    <DotLoader color="#3498db" size={50} />
+                </div>
+            ) : products && products.length > 0 ? (
+                products.map((product) => (
+                    <div key={product.id} className="food-card">
+                        
+                        <div className="img-wrapper">
+                            <img
+                                src={product.image}
+                                alt={product.name}
+                                className="product-image"
+                                onClick={() => handleProductClick(product.id)} 
+                            />
+                            <FaHeart
+                                className={`favorite-icon ${favorites.includes(product.id) ? 'favorited' : ''}`}
+                                onClick={() => toggleFavoriteHandler(product.id)} // Toggle favorite
+                            />
+                        </div>
+                        
+                        <div className="details">
+                            <h3>{product.name}</h3>
+                            <p className="calori">
+                                <FaDollarSign className="price-icon" />
+                                Price: {product.price} JD
+                            </p>
+                            
+                            <div className="time-rating">
+                                <p className="timee">
+                                    <MdLocalFireDepartment className="fire-icon" />
+                                    {product.extra} ext
+                                </p>
+                                <p className="ratee">⭐ {product.rating}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <p>No products found.</p>
+            )}
         </section>
     );
 };
