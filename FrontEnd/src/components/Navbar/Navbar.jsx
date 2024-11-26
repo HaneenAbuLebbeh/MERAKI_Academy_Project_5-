02424db2228e37ceaf462f22f5d3e9f02aa881d9
Haneen abulebbeh <haneenabulebbeh@gmail.com>
2024-11-26 19:49:04 +0300

    edit

--- FrontEnd/src/components/Navbar/Navbar.jsx
+++ FrontEnd/src/components/Navbar/Navbar.jsx
@@ -114,7 +114,7 @@ const Navbar = () => {
     color: 'inherit',
     '& .MuiInputBase-input': {
       padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
+    
       paddingLeft: `calc(1em + ${theme.spacing(4)})`,
       transition: theme.transitions.create('width'),
       width: '100%',
